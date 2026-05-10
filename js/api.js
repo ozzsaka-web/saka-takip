ef48c87cb54ece0e09fd31ee4bdfe84bb3620be5
ozzsaka-web <ozzsaka@gmail.com>
2026-05-10 16:38:34 +0300

    Add files via upload

--- js/api.js
+++ js/api.js
@@ -1,22 +1,50 @@
 const API = (() => {
 
   const CONFIG = {
-      SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbwUeHdeJIJG_mb4Iy7yIeFJMon8CJ6lx6cEj0UtMJYjZazcRABLAXq36MtiooZd9M9m/exec',
+    SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbwUeHdeJIJG_mb4Iy7yIeFJMon8CJ6lx6cEj0UtMJYjZazcRABLAXq36MtiooZd9M9m/exec', 
     SPREADSHEET_ID: '1KJ4P-BZLR3kSeaT2H7hv9FiDvHzR-tUM79trPcucKus',
     SIPARIS_KOD_PREFIX: 'STK',
   };
 
- async function _istek(action, payload = {}) {
-  const url = CONFIG.SCRIPT_URL +
-    '?action=' + encodeURIComponent(action) +
-    '&payload=' + encodeURIComponent(JSON.stringify(payload));
+  async function _istek(action, payload = {}) {
+    const body = JSON.stringify({ action, ...payload });
 
-  const res = await fetch(url, { redirect: 'follow' });
-  if (!res.ok) throw new Error('HTTP ' + res.status);
-  const json = await res.json();
-  if (json.status === 'error') throw new Error(json.message);
-  return json.data;
-}
+    let res;
+
+    try {
+      res = await fetch(CONFIG.SCRIPT_URL, {
+        method: 'POST',
+        headers: { 'Content-Type': 'text/plain;charset=utf-8' },
+        body,
+        redirect: 'follow'
+      });
+    } catch (e) {
+      const url =
+        CONFIG.SCRIPT_URL +
+        '?action=' + encodeURIComponent(action) +
+        '&payload=' + encodeURIComponent(body);
+
+      res = await fetch(url, {
+        method: 'GET',
+        redirect: 'follow'
+      });
+    }
+
+    if (!res.ok) throw new Error('HTTP ' + res.status);
+
+    const text = await res.text();
+
+    let json;
+    try {
+      json = JSON.parse(text);
+    } catch (e) {
+      throw new Error('Geçersiz yanıt: ' + text.substring(0, 100));
+    }
+
+    if (json.status === 'error') throw new Error(json.message);
+
+    return json.data;
+  }
 
   async function testBaglanti() {
     try {
@@ -57,8 +85,6 @@ const API = (() => {
     tahsilatlariGetir: (musteriAdi) => _istek('tahsilatlariGetir', { musteriAdi }),
     tahsilatSil: (id) => _istek('tahsilatSil', { id }),
     siparisSil: (siparisKodu) => _istek('siparisSil', { siparisKodu }),
-    uretimSil: (kalemId, siparisKodu) => _istek('uretimSil', { kalemId, siparisKodu }),
-    sevkSil: (kalemId, siparisKodu) => _istek('sevkSil', { kalemId, siparisKodu }),
   };
 
 })();
